8b36e60e29989d669830e6f68a6c903d0eec3e76
Madhusudhan Srinivasa <me@madhums.me>
2020-06-30 13:41:16 +0200

    render styled-components on server side

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,11 +6,34 @@ import Document, {
   NextScript,
   DocumentContext,
 } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 class VicDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
+    // https://github.com/vercel/next.js/blob/master/examples/with-styled-components/pages/_document.js
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
   }
 
   render() {
