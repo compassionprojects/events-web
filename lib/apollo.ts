faa15f06a335df12095c4b051e3ee1e6b67772e7
Madhusudhan Srinivasa <me@madhums.me>
2020-07-28 01:29:16 +0200

    use different credential values for fetch during development and production

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -1,6 +1,8 @@
 import ApolloClient, { InMemoryCache } from 'apollo-boost';
 import resolvers from './resolvers';
 
+const isDev = process.env.NODE_ENV === 'development';
+
 const API_URL =
   process.env.VIC_API_ROOT ||
   process.env.NEXT_PUBLIC_VIC_API_ROOT ||
@@ -8,7 +10,7 @@ const API_URL =
 
 const apolloClient = new ApolloClient({
   uri: API_URL,
-  credentials: 'same-origin',
+  credentials: isDev ? 'include' : 'same-origin',
   resolvers,
   cache: new InMemoryCache(),
 });
