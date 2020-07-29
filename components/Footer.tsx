d57c9b790d489ddc42d16dd9ba9865ec868cf09c
Madhusudhan Srinivasa <me@madhums.me>
2020-07-29 02:00:22 +0200

    make footer better and consistent

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,54 +1,62 @@
 import React from 'react';
 import scrollTo from 'scroll-to-element';
 import Icon from './Icon';
+import Link from './Link';
 import data from '../data';
 import { o } from './Header';
 
+const year = new Date().getFullYear();
+
 export default function FooterLanding() {
   return (
-    <footer className="footer mt-auto py-5 border-top">
+    <footer className="footer mt-auto py-5 border-top bg-light">
       <div className="container">
         <ul className="nav justify-content-center">
           <li className="nav-item">
-            <a
+            <Link
               href="/#"
+              as="/#"
               className="nav-link text-muted"
               onClick={() => scrollTo('#top', o)}>
               <Icon shape="arrow-up" className="p-1" />
               Back to top
-            </a>
+            </Link>
           </li>
           <li className="nav-item">
-            <a
+            <Link
               className="nav-link"
               href="/#about"
+              as="/#about"
               onClick={() => scrollTo('#about', o)}>
               About
-            </a>
+            </Link>
           </li>
           <li className="nav-item">
-            <a
+            <Link
               className="nav-link"
               href="/#course"
+              as="/#course"
               onClick={() => scrollTo('#course', o)}>
               Course
-            </a>
+            </Link>
           </li>
           <li className="nav-item">
-            <a
+            <Link
               className="nav-link"
               href="/#trainers"
+              as="/#trainers"
               onClick={() => scrollTo('#trainers', o)}>
               Trainers
-            </a>
+            </Link>
           </li>
           <li className="nav-item">
-            <a
+            <Link
               className="nav-link"
               href="/#faq"
+              as="/#faq"
               onClick={() => scrollTo('#faq', o)}>
               FAQ&apos;s
-            </a>
+            </Link>
           </li>
           <li className="nav-item">
             <a className="nav-link" href={`mailto:${data.contact_email}`}>
@@ -57,30 +65,32 @@ export default function FooterLanding() {
           </li>
         </ul>
       </div>
+      <div className="py-4 mt-3 small text-muted text-center">
+        &copy; {year} peacefactory.fr
+      </div>
     </footer>
   );
 }
 
 export function Footer() {
-  const year = new Date().getFullYear();
   return (
     <footer className="footer mt-auto py-5 border-top bg-light">
       <div className="container">
         <ul className="nav justify-content-center">
           <li className="nav-item">
-            <a className="nav-link" href="/#about">
+            <Link className="nav-link" href="/#about" as="/#about">
               About
-            </a>
+            </Link>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="/#course">
+            <Link className="nav-link" href="/#course" as="/#course">
               Course
-            </a>
+            </Link>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="/#faq">
+            <Link className="nav-link" href="/#faq" as="/#faq">
               FAQ&apos;s
-            </a>
+            </Link>
           </li>
           <li className="nav-item">
             <a className="nav-link" href={`mailto:${data.contact_email}`}>
