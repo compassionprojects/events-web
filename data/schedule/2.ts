0f8f95f03b113ed0f8e88d7056e7bd002d4ceee6
Madhusudhan Srinivasa <me@madhums.me>
2020-10-21 22:56:52 +0200

    update schedule

--- data/schedule/2.ts
+++ data/schedule/2.ts
@@ -15,8 +15,8 @@ function t(time_in_cest) {
 export default [
   // Day 1
   {
-    startDate: moment(`2020-08-08T09:30:00+02:00`),
-    startDateFormatted: moment(`2020-08-08T09:30:00+02:00`)
+    startDate: moment(`2020-11-13T09:30:00+02:00`),
+    startDateFormatted: moment(`2020-11-13T09:30:00+02:00`)
       .tz(tzName)
       .format('dddd, MMMM Do YYYY z'),
     schedule: [
@@ -83,8 +83,81 @@ export default [
 
   // Day 2
   {
-    startDate: moment(`2020-08-09T07:30:00+02:00`),
-    startDateFormatted: moment(`2020-08-09T07:30:00+02:00`)
+    startDate: moment(`2020-11-14T07:30:00+02:00`),
+    startDateFormatted: moment(`2020-11-14T07:30:00+02:00`)
+      .tz(tzName)
+      .format('dddd, MMMM Do YYYY z'),
+    schedule: [
+      {
+        start: t('07:30'),
+        end: t('09:15'),
+        body: `For all offerings (meditation, yoga, dance, movement)`,
+      },
+      {
+        start: t('09:30'),
+        end: t('09:45'),
+        body: `Community session (settling and logistics)`,
+      },
+      {
+        start: t('09:45'),
+        end: t('10:05'),
+        body: `Remembering and feedback linked to the Remembering`,
+      },
+      {
+        start: t('10:05'),
+        end: t('10:20'),
+        body: `Presentation of trainer offerings`,
+      },
+      { start: t('10:20'), end: t('10:35'), body: `Break` },
+      {
+        start: t('10:35'),
+        end: t('12:35'),
+        body: `<div class="d-flex justify-content-between">
+        <div>NVC in schools<br><i>Magda,  <br>4 days +</i></div>
+        <div>I am the change <br>I want to see in the world<br><i>Irmtraud,  <br>All levels</i></div>
+        <div>Mr. Rosenberg <br>and the coffee cup<br><i>Frank,<br>All levels</i></div>
+  </div>
+    `,
+      },
+      {
+        start: t('12:35'),
+        end: t('14:05'),
+        body: `Break for lunch (including time to cook and eat together)`,
+      },
+      {
+        start: t('14:05'),
+        end: t('16:05'),
+        body: `<div class="d-flex justify-content-between">
+        <div>I hate you<br><i>Godfrey,  <br>All levels</i></div>
+        <div>Gratitude<br><i>Irmtraud,  <br>All levels</i></div>
+        <div>The four themes <br>of life<br><i>Frank,  <br>5 days +</i></div>
+      </div>`,
+      },
+      { start: t('16:05'), end: t('16:20'), body: `Break` },
+      { start: t('16:20'), end: t('17:05'), body: `Home Groups` },
+      { start: t('17:05'), end: t('17:10'), body: `Back to plenary session` },
+      {
+        start: t('17:10'),
+        end: t('17:30'),
+        body: `Feedback of the day (harvesting)`,
+      },
+      {
+        start: t('17:30'),
+        end: t('17:40'),
+        body: `Community session (logistics)`,
+      },
+      {
+        start: t('17:40'),
+        end: t('22:00'),
+        body: `For all other offerings (meditation, yoga, dance, movement, ...)`,
+      },
+    ],
+  },
+
+  // Day 3
+  {
+    startDate: moment(`2020-11-15T07:30:00+02:00`),
+    startDateFormatted: moment(`2020-11-15T07:30:00+02:00`)
       .tz(tzName)
       .format('dddd, MMMM Do YYYY z'),
     schedule: [
