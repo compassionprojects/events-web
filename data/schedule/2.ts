d00cc0af15f10e23df1b8edce62200045e8a81de
Natalia Menon <natalia@peacefactory.fr>
2020-11-02 06:58:58 +0000

    Update 2.ts

--- data/schedule/2.ts
+++ data/schedule/2.ts
@@ -40,7 +40,7 @@ export default [
         start: t('10:35'),
         end: t('12:35'),
         body: `<div class="d-flex justify-content-between">
-  <div>Session option 1 1<br><i>Magda, <br>Beginners Track</i></div>
+  <div>Session option 1 <br><i>Magda, <br>Beginners Track</i></div>
   <div>Session option 2 <br><i>Irmtraud, <br> </i></div>
   <div>Session option 3 <br><i> TBA, <br> </i></div>
 </div>
@@ -55,7 +55,7 @@ export default [
         start: t('14:05'),
         end: t('16:05'),
         body: `<div class="d-flex justify-content-between">
-          <div>Session option 1 1<br><i>Louise, <br>Beginners Track</i></div>
+          <div>Session option 1 <br><i>Louise, <br>Beginners Track</i></div>
           <div>Session option 2 <br><i>Godfrey, <br> </i></div>
           <div>Session option 3 <br><i> TBA, <br> </i></div>
     </div>`,
@@ -82,14 +82,14 @@ export default [
         start: t('18:00'),
         end: t('20:00'),
         body: `<div class="d-flex justify-content-between">
-  <div>Session option 1 1<br><i> <br> </i></div>
-  <div>Session option 2 <br><i> <br> </i></div>
+  <div>Session option 1 <br><i> <br> </i>Optional </div>
+  <div>Session option 2 <br><i> <br> </i>Optional </div>
 </div>
   `,
       },
       {
         start: t('18:00'),
-        end: t('21:00'),
+        end: t('22:00'),
         body: `For all other offerings (meditation, workshops, yoga, dance, movement, ...)`,
       },
     ],
@@ -97,16 +97,11 @@ export default [
 
   // Day 2
   {
-    startDate: moment(`2020-11-14T07:30:00+02:00`),
-    startDateFormatted: moment(`2020-11-14T07:30:00+02:00`)
+    startDate: moment(`2020-11-14T09:30:00+02:00`),
+    startDateFormatted: moment(`2020-11-14T09:30:00+02:00`)
       .tz(tzName)
       .format('dddd, MMMM Do YYYY z'),
     schedule: [
-      {
-        start: t('07:30'),
-        end: t('09:15'),
-        body: `For all offerings (meditation, yoga, dance, movement)`,
-      },
       {
         start: t('09:30'),
         end: t('09:45'),
@@ -127,11 +122,11 @@ export default [
         start: t('10:35'),
         end: t('12:35'),
         body: `<div class="d-flex justify-content-between">
-        <div>NVC in schools<br><i>Magda,  <br>4 days +</i></div>
-        <div>I am the change <br>I want to see in the world<br><i>Irmtraud,  <br>All levels</i></div>
-        <div>Mr. Rosenberg <br>and the coffee cup<br><i>Frank,<br>All levels</i></div>
-  </div>
-    `,
+  <div>Session option 1 <br><i>Magda, <br>Beginners Track</i></div>
+  <div>Session option 2 <br><i>Genevieve, <br> </i></div>
+  <div>Session option 3 <br><i> TBA, <br> </i></div>
+</div>
+  `,
       },
       {
         start: t('12:35'),
@@ -142,10 +137,10 @@ export default [
         start: t('14:05'),
         end: t('16:05'),
         body: `<div class="d-flex justify-content-between">
-        <div>I hate you<br><i>Godfrey,  <br>All levels</i></div>
-        <div>Gratitude<br><i>Irmtraud,  <br>All levels</i></div>
-        <div>The four themes <br>of life<br><i>Frank,  <br>5 days +</i></div>
-      </div>`,
+          <div>Session option 1 <br><i>Louise, <br>Beginners Track</i></div>
+          <div>Session option 2 <br><i>Godfrey, <br> </i></div>
+          <div>Session option 3 <br><i> TBA, <br> </i></div>
+    </div>`,
       },
       { start: t('16:05'), end: t('16:20'), body: `Break` },
       { start: t('16:20'), end: t('17:05'), body: `Home Groups` },
@@ -162,24 +157,33 @@ export default [
       },
       {
         start: t('17:40'),
+        end: t('18:00'),
+        body: `Trainers meeting, from participant requests to offerings`,
+      },
+      {
+        start: t('18:00'),
+        end: t('20:00'),
+        body: `<div class="d-flex justify-content-between">
+  <div>Session option 1 <br><i> <br> </i>Optional </div>
+  <div>Session option 2 <br><i> <br> </i>Optional </div>
+</div>
+  `,
+      },
+      {
+        start: t('18:00'),
         end: t('22:00'),
-        body: `For all other offerings (meditation, yoga, dance, movement, ...)`,
+        body: `For all other offerings (meditation, workshops, yoga, dance, movement, ...)`,
       },
     ],
   },
 
   // Day 3
   {
-    startDate: moment(`2020-11-15T07:30:00+02:00`),
-    startDateFormatted: moment(`2020-11-15T07:30:00+02:00`)
+    startDate: moment(`2020-11-15T09:30:00+02:00`),
+    startDateFormatted: moment(`2020-11-15T09:30:00+02:00`)
       .tz(tzName)
       .format('dddd, MMMM Do YYYY z'),
     schedule: [
-      {
-        start: t('07:30'),
-        end: t('09:15'),
-        body: `For all offerings (meditation, yoga, dance, movement)`,
-      },
       {
         start: t('09:30'),
         end: t('09:45'),
@@ -200,11 +204,11 @@ export default [
         start: t('10:35'),
         end: t('12:35'),
         body: `<div class="d-flex justify-content-between">
-        <div>NVC in schools<br><i>Magda,  <br>4 days +</i></div>
-        <div>I am the change <br>I want to see in the world<br><i>Irmtraud,  <br>All levels</i></div>
-        <div>Mr. Rosenberg <br>and the coffee cup<br><i>Frank,<br>All levels</i></div>
-  </div>
-    `,
+  <div>Session option 1 <br><i>Magda, <br>Beginners Track</i></div>
+  <div>Session option 2 <br><i>Irmtraud, <br> </i></div>
+  <div>Session option 3 <br><i> Frank, <br> </i></div>
+</div>
+  `,
       },
       {
         start: t('12:35'),
@@ -215,10 +219,10 @@ export default [
         start: t('14:05'),
         end: t('16:05'),
         body: `<div class="d-flex justify-content-between">
-        <div>I hate you<br><i>Godfrey,  <br>All levels</i></div>
-        <div>Gratitude<br><i>Irmtraud,  <br>All levels</i></div>
-        <div>The four themes <br>of life<br><i>Frank,  <br>5 days +</i></div>
-      </div>`,
+          <div>Session option 1 <br><i>Louise, <br>Beginners Track</i></div>
+          <div>Session option 2 <br><i>Irmtraud, <br> </i></div>
+          <div>Session option 3 <br><i> Frank, <br> </i></div>
+    </div>`,
       },
       { start: t('16:05'), end: t('16:20'), body: `Break` },
       { start: t('16:20'), end: t('17:05'), body: `Home Groups` },
@@ -235,9 +239,419 @@ export default [
       },
       {
         start: t('17:40'),
-        end: t('22:00'),
-        body: `For all other offerings (meditation, yoga, dance, movement, ...)`,
+        end: t('18:00'),
+        body: `Trainers meeting, from participant requests to offerings`,
       },
+      {
+        start: t('18:00'),
+        end: t('20:00'),
+        body: `<div class="d-flex justify-content-between">
+  <div>Session option 1 <br><i>Godfrey <br> </i>Optional </div>
+  <div>Session option 2 <br><i> <br> </i>Optional </div>
+</div>
+  `,
+      },
+      {
+        start: t('18:00'),
+        end: t('22:00'),
+        body: `For all other offerings (meditation, workshops, yoga, dance, movement, ...)`,
+      },
+    ],
+  },
+  // Day off
+  {
+    startDate: moment(`2020-11-16T09:30:00+02:00`),
+    startDateFormatted: moment(`2020-11-16T09:30:00+02:00`)
+      .tz(tzName)
+      .format('dddd, MMMM Do YYYY z'),
+    schedule: [
+      {
+        start: t('09:30'),
+        end: t('22:00'),
+        body: `Day Off`,
+      },
+      
+    ],
+  },
+    // Day 4
+  {
+    startDate: moment(`2020-11-17T09:30:00+02:00`),
+    startDateFormatted: moment(`2020-11-17T09:30:00+02:00`)
+      .tz(tzName)
+      .format('dddd, MMMM Do YYYY z'),
+    schedule: [
+      {
+        start: t('09:30'),
+        end: t('09:45'),
+        body: `Community session (settling and logistics)`,
+      },
+      {
+        start: t('09:45'),
+        end: t('10:05'),
+        body: `Remembering and feedback linked to the Remembering`,
+      },
+      {
+        start: t('10:05'),
+        end: t('10:20'),
+        body: `Presentation of trainer offerings`,
+      },
+      { start: t('10:20'), end: t('10:35'), body: `Break` },
+      {
+        start: t('10:35'),
+        end: t('12:35'),
+        body: `<div class="d-flex justify-content-between">
+  <div>Session option 1 <br><i>Magda, <br></i></div>
+  <div>Session option 2 <br><i>Irmtraud, <br> </i></div>
+  <div>Session option 3 <br><i> Frank, <br> </i></div>
+</div>
+  `,
+      },
+      {
+        start: t('12:35'),
+        end: t('14:05'),
+        body: `Break for lunch (including time to cook and eat together)`,
+      },
+      {
+        start: t('14:05'),
+        end: t('16:05'),
+        body: `<div class="d-flex justify-content-between">
+          <div>Session option 1 <br><i>Louise, <br> </i></div>
+          <div>Session option 2 <br><i>Irmtraud, <br> </i></div>
+          <div>Session option 3 <br><i> Frank, <br> </i></div>
+    </div>`,
+      },
+      { start: t('16:05'), end: t('16:20'), body: `Break` },
+      { start: t('16:20'), end: t('17:05'), body: `Home Groups` },
+      { start: t('17:05'), end: t('17:10'), body: `Back to plenary session` },
+      {
+        start: t('17:10'),
+        end: t('17:30'),
+        body: `Feedback of the day (harvesting)`,
+      },
+      {
+        start: t('17:30'),
+        end: t('17:40'),
+        body: `Community session (logistics)`,
+      },
+      {
+        start: t('17:40'),
+        end: t('18:00'),
+        body: `Trainers meeting, from participant requests to offerings`,
+      },
+      {
+        start: t('18:00'),
+        end: t('20:00'),
+        body: `<div class="d-flex justify-content-between">
+  <div>About Certification <br><i>Irmtraud & Frank <br> </i>Optional </div>
+  <div>Session option 2 <br><i>Godfrey <br> </i>Optional </div>
+</div>
+  `,
+      },
+      {
+        start: t('18:00'),
+        end: t('22:00'),
+        body: `For all other offerings (meditation, workshops, yoga, dance, movement, ...)`,
+      },
+    ],
+  },
+  // Day 5
+  {
+    startDate: moment(`2020-11-18T09:30:00+02:00`),
+    startDateFormatted: moment(`2020-11-18T09:30:00+02:00`)
+      .tz(tzName)
+      .format('dddd, MMMM Do YYYY z'),
+    schedule: [
+      {
+        start: t('09:30'),
+        end: t('09:45'),
+        body: `Community session (settling and logistics)`,
+      },
+      {
+        start: t('09:45'),
+        end: t('10:05'),
+        body: `Remembering and feedback linked to the Remembering`,
+      },
+      {
+        start: t('10:05'),
+        end: t('10:20'),
+        body: `Presentation of trainer offerings`,
+      },
+      { start: t('10:20'), end: t('10:35'), body: `Break` },
+      {
+        start: t('10:35'),
+        end: t('12:35'),
+        body: `<div class="d-flex justify-content-between">
+  <div>Session option 1 <br><i>Magda, <br></i></div>
+  <div>Session option 2 <br><i>Irmtraud, <br> </i></div>
+  <div>Session option 3 <br><i> Frank, <br> </i></div>
+</div>
+  `,
+      },
+      {
+        start: t('12:35'),
+        end: t('14:05'),
+        body: `Break for lunch (including time to cook and eat together)`,
+      },
+      {
+        start: t('14:05'),
+        end: t('16:05'),
+        body: `<div class="d-flex justify-content-between">
+          <div>Session option 1 <br><i>Louise, <br> </i></div>
+          <div>Session option 2 <br><i>Irmtraud, <br> </i></div>
+          <div>Session option 3 <br><i> Frank, <br> </i></div>
+    </div>`,
+      },
+      { start: t('16:05'), end: t('16:20'), body: `Break` },
+      { start: t('16:20'), end: t('17:05'), body: `Home Groups` },
+      { start: t('17:05'), end: t('17:10'), body: `Back to plenary session` },
+      {
+        start: t('17:10'),
+        end: t('17:30'),
+        body: `Feedback of the day (harvesting)`,
+      },
+      {
+        start: t('17:30'),
+        end: t('17:40'),
+        body: `Community session (logistics)`,
+      },
+      {
+        start: t('17:40'),
+        end: t('18:00'),
+        body: `Trainers meeting, from participant requests to offerings`,
+      },
+      {
+        start: t('18:00'),
+        end: t('20:00'),
+        body: `<div class="d-flex justify-content-between">
+  <div>Session option 1 <br><i>Godfrey <br> </i>Optional </div>
+  <div>Session option 2 <br><i> <br> </i>Optional </div>
+</div>
+  `,
+      },
+      {
+        start: t('18:00'),
+        end: t('22:00'),
+        body: `For all other offerings (meditation, workshops, yoga, dance, movement, ...)`,
+      },
+    ],
+  },
+  // Day off
+  {
+    startDate: moment(`2020-11-19T09:30:00+02:00`),
+    startDateFormatted: moment(`2020-11-19T09:30:00+02:00`)
+      .tz(tzName)
+      .format('dddd, MMMM Do YYYY z'),
+    schedule: [
+      {
+        start: t('09:30'),
+        end: t('22:00'),
+        body: `Day Off`,
+      },
+      
+    ],
+  },
+  // Day 6
+  {
+    startDate: moment(`2020-11-20T09:30:00+02:00`),
+    startDateFormatted: moment(`2020-11-20T09:30:00+02:00`)
+      .tz(tzName)
+      .format('dddd, MMMM Do YYYY z'),
+    schedule: [
+      {
+        start: t('09:30'),
+        end: t('09:45'),
+        body: `Community session (settling and logistics)`,
+      },
+      {
+        start: t('09:45'),
+        end: t('10:05'),
+        body: `Remembering and feedback linked to the Remembering`,
+      },
+      {
+        start: t('10:05'),
+        end: t('10:20'),
+        body: `Presentation of trainer offerings`,
+      },
+      { start: t('10:20'), end: t('10:35'), body: `Break` },
+      {
+        start: t('10:35'),
+        end: t('12:35'),
+        body: `<div class="d-flex justify-content-between">
+  <div>Session option 1 <br><i>Magda, <br></i></div>
+  <div>Session option 2 <br><i>Irmtraud, <br> </i></div>
+  <div>Session option 3 <br><i> Frank, <br> </i></div>
+</div>
+  `,
+      },
+      {
+        start: t('12:35'),
+        end: t('14:05'),
+        body: `Break for lunch (including time to cook and eat together)`,
+      },
+      {
+        start: t('14:05'),
+        end: t('16:05'),
+        body: `<div class="d-flex justify-content-between">
+          <div>Session option 1 <br><i>Louise, <br> </i></div>
+          <div>Session option 2 <br><i>Irmtraud, <br> </i></div>
+          <div>Session option 3 <br><i> Frank, <br> </i></div>
+    </div>`,
+      },
+      { start: t('16:05'), end: t('16:20'), body: `Break` },
+      { start: t('16:20'), end: t('17:05'), body: `Home Groups` },
+      { start: t('17:05'), end: t('17:10'), body: `Back to plenary session` },
+      {
+        start: t('17:10'),
+        end: t('17:30'),
+        body: `Feedback of the day (harvesting)`,
+      },
+      {
+        start: t('17:30'),
+        end: t('17:40'),
+        body: `Community session (logistics)`,
+      },
+      {
+        start: t('17:40'),
+        end: t('18:00'),
+        body: `Trainers meeting, from participant requests to offerings`,
+      },
+      {
+        start: t('18:00'),
+        end: t('20:00'),
+        body: `<div class="d-flex justify-content-between">
+  <div>Session option 1 <br><i>Godfrey <br> </i>Optional </div>
+  <div>Session option 2 <br><i> <br> </i>Optional </div>
+</div>
+  `,
+      },
+      {
+        start: t('18:00'),
+        end: t('22:00'),
+        body: `For all other offerings (meditation, workshops, yoga, dance, movement, ...)`,
+      },
+    ],
+  },
+  // Day 7
+  {
+    startDate: moment(`2020-11-21T09:30:00+02:00`),
+    startDateFormatted: moment(`2020-11-21T09:30:00+02:00`)
+      .tz(tzName)
+      .format('dddd, MMMM Do YYYY z'),
+    schedule: [
+      {
+        start: t('09:30'),
+        end: t('09:45'),
+        body: `Community session (settling and logistics)`,
+      },
+      {
+        start: t('09:45'),
+        end: t('10:05'),
+        body: `Remembering and feedback linked to the Remembering`,
+      },
+      {
+        start: t('10:05'),
+        end: t('10:20'),
+        body: `Presentation of trainer offerings`,
+      },
+      { start: t('10:20'), end: t('10:35'), body: `Break` },
+      {
+        start: t('10:35'),
+        end: t('12:35'),
+        body: `<div class="d-flex justify-content-between">
+  <div>Session option 1 <br><i>Magda, <br></i></div>
+  <div>Session option 2 <br><i>Irmtraud, <br> </i></div>
+  <div>Session option 3 <br><i> <br> </i></div>
+</div>
+  `,
+      },
+      {
+        start: t('12:35'),
+        end: t('14:05'),
+        body: `Break for lunch (including time to cook and eat together)`,
+      },
+      {
+        start: t('14:05'),
+        end: t('16:05'),
+        body: `<div class="d-flex justify-content-between">
+          <div>Money pile <br><i>Louise, <br> </i></div>
+          <div>Session option 2 <br><i>Irmtraud, <br> </i></div>
+          <div>Session option 3 <br><i>Godfrey <br> </i></div>
+    </div>`,
+      },
+      { start: t('16:05'), end: t('16:20'), body: `Break` },
+      { start: t('16:20'), end: t('17:05'), body: `Home Groups` },
+      { start: t('17:05'), end: t('17:10'), body: `Back to plenary session` },
+      {
+        start: t('17:10'),
+        end: t('17:30'),
+        body: `Feedback of the day (harvesting)`,
+      },
+      {
+        start: t('17:30'),
+        end: t('17:40'),
+        body: `Community session (logistics)`,
+      },
+      {
+        start: t('17:40'),
+        end: t('18:00'),
+        body: `Break`,
+      },
+      {
+        start: t('18:00'),
+        end: t('21:00'),
+        body: `Community time`
+  
+      },
+      
+    ],
+  },
+  // Day 8
+  {
+    startDate: moment(`2020-11-22T09:30:00+02:00`),
+    startDateFormatted: moment(`2020-11-22T09:30:00+02:00`)
+      .tz(tzName)
+      .format('dddd, MMMM Do YYYY z'),
+    schedule: [
+      {
+        start: t('09:30'),
+        end: t('09:45'),
+        body: `Community session (settling and logistics)`,
+      },
+      {
+        start: t('09:45'),
+        end: t('10:05'),
+        body: `Remembering and feedback linked to the Remembering`,
+      },
+      {
+        start: t('10:05'),
+        end: t('10:20'),
+        body: `Explain the day`,
+      },
+      { start: t('10:20'), end: t('10:35'), body: `Break` },
+      {
+        start: t('10:35'),
+        end: t('12:35'),
+        body: `<div class="d-flex justify-content-between">
+  <div>Landing after the course <br><i>Irmtraud, <br></i></div>
+  <div>Landing after the course <br><i>Louise, <br> </i></div>
+  <div>Session option 3 <br><i>Magdalena <br> </i></div>
+</div>
+  `,
+      },
+      {
+        start: t('12:35'),
+        end: t('14:05'),
+        body: `Break for lunch (including time to cook and eat together)`,
+      },
+      {
+        start: t('14:05'),
+        end: t('16:05'),
+        body: `Home Groups`
+      },
+      { start: t('16:05'), end: t('16:20'), body: `Break` },
+      { start: t('16:20'), end: t('17:40'), body: `Harvesting` },
+      { start: t('17:40'), end: t('19:30'), body: `Space for participants` },
+      
     ],
   },
 ];
