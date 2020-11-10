df10a3f34cf4b5d96225dbde60ace8758b6c5741
Madhusudhan Srinivasa <me@madhums.me>
2020-11-10 15:02:55 +0100

    update footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import scrollTo from 'scroll-to-element';
-import Icon from './Icon';
 import Link from './Link';
 import data from '../data/landing';
 import { o } from './Header';
@@ -13,7 +12,7 @@ export default function Footer() {
     <footer className="footer mt-auto py-5 border-top bg-light">
       <div className="container mt-4">
         <div className="row">
-          <ul className="my-3 list-unstyled col-md-3 col-sm-12">
+          <ul className="my-3 list-unstyled col-md-3 col-sm-12 order-sm-1 order-md-0">
             <li className="my-2">
               <Link href="/about" as="/about">
                 About us
@@ -50,32 +49,46 @@ export default function Footer() {
             </li>
           </ul>
 
-          <ul className="my-3 list-unstyled col-md-3 col-sm-12">
-            <li className="my-1">
+          <div className="my-3 col-md-6 col-lg-5 col-sm-12 pb-4 pb-md-0 order-sm-0 order-md-1">
+            <h5 className="text-muted">Subscribe to our newsletter</h5>
+            <p>Keep me updated on Peace Factory news.</p>
+            <Subscribe id="7315a21229" />
+          </div>
+
+          <ul className="my-3 list-inline col-md-3 col-sm-12 ml-auto order-2">
+            <li className="list-inline-item">
               <a
                 href="https://www.facebook.com/Peace-Factory-563809806979098/"
                 target="_blank"
                 rel="noreferrer">
-                <Icon shape="facebook" className="py-1" /> Facebook
+                <img src="/images/icon-facebook.svg" height={30} width={30} />
               </a>
             </li>
-            <li className="my-1">
+            <li className="list-inline-item">
+              <a
+                href="https://www.instagram.com/p/CHFzXGpKV0C/"
+                target="_blank"
+                rel="noreferrer">
+                <img src="/images/icon-instagram.svg" height={30} width={30} />
+              </a>
+            </li>
+            <li className="list-inline-item">
+              <a
+                href="https://www.youtube.com/channel/UCtBY3xN-CtE_-x3rnHgTg_Q"
+                target="_blank"
+                rel="noreferrer">
+                <img src="/images/icon-youtube.svg" height={30} width={30} />
+              </a>
+            </li>
+            <li className="list-inline-item">
               <a
                 href="https://www.linkedin.com/company/peace-factory/"
                 target="_blank"
                 rel="noreferrer">
-                <Icon shape="linkedin" className="py-1" /> Linked In
+                <img src="/images/icon-linkedin.svg" height={30} width={30} />
               </a>
             </li>
           </ul>
-
-          <div className="my-3 col-md-6 col-sm-12">
-            <h5 className="font-weight-bold">Subscribe to our newsletter</h5>
-            <p>
-              Keep me updated on Peace Factory news.
-            </p>
-            <Subscribe id="7315a21229" />
-          </div>
         </div>
       </div>
 
