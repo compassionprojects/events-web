7db26074fe5d80ad4b7f5ff9b10747903face55f
madhums <me@madhums.me>
2020-12-04 18:43:50 +0000

    Update index.ts

--- data/schedule/index.ts
+++ data/schedule/index.ts
@@ -1,4 +1,5 @@
 import course1 from './1';
 import course2 from './2';
+import course3 from './3';
 
-export default { 1: course1, 2: course2 };
+export default { 1: course1, 2: course2, 3: course3 };
