1557f5b596738cb438511c445db505820ae2e163
Natalia Menon <natalia@peacefactory.fr>
2020-12-05 08:22:59 +0000

    Update 3.ts

--- data/schedule/3.ts
+++ data/schedule/3.ts
@@ -16,7 +16,141 @@ export default [
   // Day 1
   {
     startDate: moment(`2020-12-18T05:30:00+02:00`),
-    startDateFormatted: moment(`2020-11-13T05:30:00+02:00`)
+    startDateFormatted: moment(`2020-12-18T05:30:00+02:00`)
+      .tz(tzName)
+      .format('dddd, MMMM Do YYYY z'),
+    schedule: [
+      {
+        start: t('05:30'),
+        end: t('06:15'),
+        body: `Community session (settling and logistics)`,
+      },
+      { start: t('06:15'), end: t('06:30'), body: `Break`, type: 'break' },
+      {
+        start: t('06:30'),
+        end: t('08:00'),
+        body: `<div class="d-flex justify-content-between">
+          <div>Session 1 <br><i><br>Beginners Track</i><br>
+          </div>
+          <div>Session 2 <br><i><br></i>
+          </div>
+          <div>Session 3 <br><i>Nic<br></i>
+          </div>
+    </div>`,
+      },
+      {
+        start: t('08:00'),
+        end: t('09:30'),
+        body: `Break for lunch`,
+        type: 'break',
+      },
+      {
+        start: t('09:30'),
+        end: t('11:30'),
+        body: `<div class="d-flex justify-content-between">
+          <div>Session 4 <br><i><br>Beginners Track</i><br>
+          </div>
+          <div>Session 5 <br><i>Matthew<br></i>
+          </div>
+          <div>Session 6 <br><i>Nic<br></i>
+          </div>
+    </div>`,
+      },
+      { start: t('11:30'), end: t('12:00'), body: `Break`, type: 'break' },
+      {
+        start: t('12:00'),
+        end: t('14:00'),
+        body: `<div class="d-flex justify-content-between">
+          <div>Session 7 <br><i><br>Beginners Track</i><br>
+          </div>
+          <div>Session 8 <br><i><br></i>
+          </div>
+          <div>Session 9 <br><i>Nic<br></i>
+          </div>
+    </div>`,
+      },
+      {
+        start: t('14:00'),
+        end: t('14:15'),
+        body: `Break`, type: 'break' },
+      {
+        start: t('14:15'),
+        end: t('15:00'),
+        body: `Feedback of the day (harvesting)`,
+      },
+    ],
+  },
+   // Day 2
+  {
+    startDate: moment(`2020-12-19T05:30:00+02:00`),
+    startDateFormatted: moment(`2020-12-19T05:30:00+02:00`)
+      .tz(tzName)
+      .format('dddd, MMMM Do YYYY z'),
+    schedule: [
+      {
+        start: t('05:30'),
+        end: t('06:15'),
+        body: `Community session (settling and logistics)`,
+      },
+      { start: t('06:15'), end: t('06:30'), body: `Break`, type: 'break' },
+      {
+        start: t('06:30'),
+        end: t('08:00'),
+        body: `<div class="d-flex justify-content-between">
+          <div>Session 1 <br><i><br>Beginners Track</i><br>
+          </div>
+          <div>Session 2 <br><i><br></i>
+          </div>
+          <div>Session 3 <br><i>Nic<br></i>
+          </div>
+    </div>`,
+      },
+      {
+        start: t('08:00'),
+        end: t('09:30'),
+        body: `Break for lunch`,
+        type: 'break',
+      },
+      {
+        start: t('09:30'),
+        end: t('11:30'),
+        body: `<div class="d-flex justify-content-between">
+          <div>Session 4 <br><i><br>Beginners Track</i><br>
+          </div>
+          <div>Session 5 <br><i>Matthew<br></i>
+          </div>
+          <div>Session 6 <br><i>Nic<br></i>
+          </div>
+    </div>`,
+      },
+      { start: t('11:30'), end: t('12:00'), body: `Break`, type: 'break' },
+      {
+        start: t('12:00'),
+        end: t('14:00'),
+        body: `<div class="d-flex justify-content-between">
+          <div>Session 7 <br><i><br>Beginners Track</i><br>
+          </div>
+          <div>Session 8 <br><i><br></i>
+          </div>
+          <div>Session 9 <br><i>Nic<br></i>
+          </div>
+    </div>`,
+      },
+      {
+        start: t('14:00'),
+        end: t('14:15'),
+        body: `Break`, type: 'break' },
+      {
+        start: t('14:15'),
+        end: t('15:00'),
+        body: `Feedback of the day (harvesting)`,
+      },
+    ],
+  },
+   // Day 3
+  {
+    startDate: moment(`2020-12-20T05:30:00+02:00`),
+    startDateFormatted: moment(`2020-12-20T05:30:00+02:00`)
       .tz(tzName)
       .format('dddd, MMMM Do YYYY z'),
     schedule: [
