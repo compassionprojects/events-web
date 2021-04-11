ec6a07d5423cbd73a47bd0536af4dd0cebc1376f
madhums <me@madhums.me>
2021-04-11 20:46:22 +0000

    add redirect of sign in page

--- next.config.js
+++ next.config.js
@@ -6,6 +6,11 @@ module.exports = {
         destination: '/course/2/tickets',
         permanent: true,
       },
+      {
+        source: '/signin',
+        destination: '/en/signin',
+        permanent: true,
+      },
     ];
   },
 };
